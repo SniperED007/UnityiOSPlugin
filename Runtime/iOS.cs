e052aa21444922f2b0def5b4535ea2f64ad2ad25
Edward Dennekamp <edward.dennekamp@gmail.com>
2025-03-05 20:47:51 +0000

    Update iOS.cs

--- Runtime/iOS.cs
+++ Runtime/iOS.cs
@@ -8,27 +8,30 @@ public enum StatusBarStyle
   Dark = 2
 }
 
-public static class iOS
+namespace LighthouseGames
 {
+  public static class iOS
+  {
 #if UNITY_IOS && !UNITY_EDITOR
     [DllImport("__Internal")] private static extern void _SetStatusBarVisible(bool visible);
     [DllImport("__Internal")] private static extern void _SetStatusBarStyle(int style);
 #endif
 
-  public static bool ShowStatusBar
-  {
-    set
+    public static bool ShowStatusBar
     {
+      set
+      {
 #if UNITY_IOS && !UNITY_EDITOR
         _SetStatusBarVisible(value);
 #endif
+      }
     }
-  }
 
-  public static void SetStatusBarStyle(StatusBarStyle style)
-  {
+    public static void SetStatusBarStyle(StatusBarStyle style)
+    {
 #if UNITY_IOS && !UNITY_EDITOR
       _SetStatusBarStyle((int)style);
 #endif
+    }
   }
 }
